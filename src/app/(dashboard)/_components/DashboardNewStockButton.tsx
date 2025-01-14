51f3119dd4e67ca5204a63f20359fa48d62636e6
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2025-01-14 17:47:28 -0300

    feat: update AddNewStockButton to use 'dashed' variant and enhance button styling

--- src/app/(dashboard)/_components/DashboardNewStockButton.tsx
+++ src/app/(dashboard)/_components/DashboardNewStockButton.tsx
@@ -3,7 +3,7 @@ import { Button } from "../../../components/ui/button"
 
 export const AddNewStockButton = () => {
   return (
-    <Button variant="outline" className="border-dashed border-[1px] w-[300px] h-[300px]">
+    <Button variant="dashed" className="w-[300px] h-[300px]">
       <PlusIcon />
     </Button>
   )
