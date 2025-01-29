9f7d782bf4925e9da23e81a28cfb85326f5f6e00
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2025-01-29 12:07:54 -0300

    feat: enable prefetching for Sidebar navigation links to improve performance

--- src/components/Sidebar/SidebarNavigation.tsx
+++ src/components/Sidebar/SidebarNavigation.tsx
@@ -8,22 +8,22 @@ const Navigation = () => {
     <div className="flex items-center flex-col gap-6">
       <Logo />
       <Button variant="default" className="hover:bg-background py-6 w-14 mt-6" asChild>
-        <Link href="/dashboard">
+        <Link href="/dashboard" prefetch>
           <Image src="/icons/dashboard-icon.png" alt="Dashboard" width={35} height={35} />
         </Link>
       </Button>
       <Button variant="default" className="py-6 w-14 hover:bg-background" asChild>
-        <Link href="/stock" >
+        <Link href="/stock" prefetch>
           <Image src="/icons/stock-icon.png" alt="stock" width={35} height={35} className="mr-1" />
         </Link>
       </Button>
       <Button variant="default" className="hover:bg-background py-6 w-14 " asChild>
-        <Link href="/analytics">
+        <Link href="/analytics" prefetch>
           <Image src="/icons/barchart-icon.png" alt="analytics" width={35} height={35} />
         </Link>
       </Button>
       <Button variant="default" className="hover:bg-background py-6 w-14 " asChild>
-        <Link href="/settings">
+        <Link href="/settings" prefetch>
           <Image src="/icons/settings-icon.png" alt="settings" width={35} height={35} />
         </Link>
       </Button>
