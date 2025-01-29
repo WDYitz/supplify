d7eb4901760cfbbc92d1c075e89235b9ae4d6cb1
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2025-01-29 12:07:47 -0300

    feat: add loading component for Dashboard page to enhance user experience

---
+++ src/app/dashboard/loading.tsx
@@ -0,0 +1,11 @@
+import { Loader2 } from "lucide-react";
+
+const LoadingDashboardPage = () => {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <Loader2 className="animate-spin" />
+    </div>
+  );
+}
+
+export default LoadingDashboardPage;
