35d007e554c16c0b6f0254214f79ff90d55288c9
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2025-01-30 14:54:13 -0300

    feat: implement authentication middleware to manage access to routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,42 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse, type MiddlewareConfig, type NextRequest } from "next/server";
+
+const publicRoutes = [
+  { path: "/login", whenAuthenticated: "redirect" },
+] as const
+
+const REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE = '/login'
+const REDIRECT_WHEN_AUTHENTICATED_ROUTE = "/"
+
+export const middleware = (request: NextRequest) => {
+  const path = request.nextUrl.pathname
+  const publicRoute = publicRoutes.find(route => route.path === path)
+  const authToken = request.cookies.get('__session') // Clerk session cookie
+
+  if (!authToken && !publicRoute) {
+    const redirectUrl = request.nextUrl.clone()
+    redirectUrl.pathname = REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE
+
+    return NextResponse.redirect(redirectUrl)
+  }
+
+  if (authToken && publicRoutes && publicRoute?.whenAuthenticated === 'redirect') {
+    const redirectUrl = request.nextUrl.clone()
+    redirectUrl.pathname = REDIRECT_WHEN_AUTHENTICATED_ROUTE
+
+    return NextResponse.redirect(redirectUrl)
+  }
+
+  if (authToken && !publicRoute) {
+    return NextResponse.next()
+  }
+
+  return NextResponse.next()
+}
 
 export default clerkMiddleware();
 
-export const config = {
+export const config: MiddlewareConfig = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
