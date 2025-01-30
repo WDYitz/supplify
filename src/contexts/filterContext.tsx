63fe63a84c0bb2369768e7b873bcbbd0abd06f75
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2025-01-30 16:11:18 -0300

    feat: enhance filter context to include classification state management

--- src/contexts/filterContext.tsx
+++ src/contexts/filterContext.tsx
@@ -2,6 +2,12 @@
 import { usePathname, useRouter } from "next/navigation";
 import { createContext, useState, type ChangeEvent, type PropsWithChildren } from "react";
 
+type FilterClassificationType = {
+  A?: boolean,
+  B?: boolean,
+  C?: boolean
+}
+
 interface IFilterContext {
   unitValue: number[]
   setUnitValue: (value: number[]) => void
@@ -11,6 +17,8 @@ interface IFilterContext {
   handleSearch: () => void
   handleSearchValueChange: (e: ChangeEvent<HTMLInputElement>) => void
   clearFilters: () => void
+  classification: FilterClassificationType
+  setClassification: (value: FilterClassificationType) => void
   MIN_PRICE: number
   MAX_PRICE: number
   MIN_QTD: number
@@ -27,14 +35,15 @@ export const FilterProvider = ({ children }: PropsWithChildren) => {
   const [unitValue, setUnitValue] = useState([MIN_PRICE, MAX_PRICE]);
   const [qtdValue, setQtdValue] = useState([MIN_QTD, MAX_QTD]);
   const [search, setSearch] = useState<string>("");
+  const [classification, setClassification] = useState<FilterClassificationType>({ A: false, B: false, C: false });
   const pathName = usePathname();
   const router = useRouter();
 
   const handleSearch = () => {
     if (search.length > 0) {
-      return router.push(`${pathName}/?search=${search}&min-price=${unitValue[0]}&max-price=${unitValue[1]}&min-qtd=${qtdValue[0]}&max-qtd=${qtdValue[1]}`)
+      return router.push(`${pathName}/?search=${search}&min-price=${unitValue[0]}&max-price=${unitValue[1]}&min-qtd=${qtdValue[0]}&max-qtd=${qtdValue[1]}&classification=${Object.keys(classification).filter((key) => classification[key as keyof FilterClassificationType]).join(",")}`)
     }
-    return router.push(`${pathName}/?min-price=${unitValue[0]}&max-price=${unitValue[1]}&min-qtd=${qtdValue[0]}&max-qtd=${qtdValue[1]}`)
+    return router.push(`${pathName}/?min-price=${unitValue[0]}&max-price=${unitValue[1]}&min-qtd=${qtdValue[0]}&max-qtd=${qtdValue[1]}&classification=${Object.keys(classification).filter((key) => classification[key as keyof FilterClassificationType]).join(",")}`)
   }
 
   const handleSearchValueChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -46,10 +55,11 @@ export const FilterProvider = ({ children }: PropsWithChildren) => {
     setUnitValue([MIN_PRICE, MAX_PRICE]);
     setQtdValue([MIN_QTD, MAX_QTD]);
     setSearch("");
+    setClassification({ A: false, B: false, C: false });
   }
 
   return (
-    <FilterContext.Provider value={{ unitValue, setUnitValue, qtdValue, setQtdValue, search, handleSearch, MIN_PRICE, MAX_PRICE, MIN_QTD, MAX_QTD, handleSearchValueChange, clearFilters }}>
+    <FilterContext.Provider value={{ unitValue, setUnitValue, qtdValue, setQtdValue, search, handleSearch, MIN_PRICE, MAX_PRICE, MIN_QTD, MAX_QTD, handleSearchValueChange, clearFilters, setClassification, classification }}>
       {children}
     </FilterContext.Provider>
   )
