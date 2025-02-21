1eae0c3180e3d6ee1c8ea7fb11c3fdfc583b9e3e
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2025-02-21 15:58:21 -0300

    feat: integrate useSearchParams for improved stock table functionality

--- src/app/stock/_components/Stock/StockTable.tsx
+++ src/app/stock/_components/Stock/StockTable.tsx
@@ -2,9 +2,11 @@ import { ProductsTable } from "@/components/ProductsTable";
 import { productsColumns } from "@/components/ProductsTable/ProductsColumns";
 import { productsMock } from "@/mocks/product";
 import ProductDoesNotExist from "../ProductDoesNotExist";
-
+import { useSearchParams } from "next/navigation";
 
 const StockTable = () => {
+
+
   if (productsMock.length <= 0) {
     return <ProductDoesNotExist />
   }
